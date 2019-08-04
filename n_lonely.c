93d94d0ceb4aacd7cc3767d297c407638ed65b40
Mayank kainth <mayank171473.cse@chitkara.edu.in>
2019-08-05 01:11:04 +0530

    n lonely numbers using -1... wrong logic

---
+++ n_lonely.c
@@ -0,0 +1,33 @@
+//this code output is not correct in all test cases such as when we put length 7 an array 1 2 3 4 1 2 3 the output should be 4 but it gives 3.....
+// And this was also valid only if 0<a[i]<n which a[i] means value at index in array......
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int *a=(int *)calloc(n,sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",a+i);
+    }
+    for(i=0;i<n;i++)
+    {
+        *(a+*(a+i))=*(a+*(a+i)) * -1;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d",*(a+i));
+    }
+    for(i=0;i<n;i++)
+    {
+        if(*(a+i)<0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
