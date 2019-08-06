e34407507e1ae9ae89b4d6a9dad98b3ed59f6c86
Mayank kainth <mayank171473.cse@chitkara.edu.in>
2019-08-06 10:23:43 +0530

    n_lonely corrected

--- n_lonely.c
+++ n_lonely.c
@@ -1,13 +1,13 @@
 //this code output is not correct in all test cases such as when we put length 7 an array 1 2 3 4 1 2 3 the output should be 4 but it gives 3.....
 // And this was also valid only if 0<a[i]<n which a[i] means value at index in array......
-
+//it was because i didn't took mod of the value at respected index...
 
 
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-    int i,n;
+    int i,n,m;
     scanf("%d",&n);
     int *a=(int *)calloc(n,sizeof(int));
     for(i=0;i<n;i++)
@@ -16,11 +16,10 @@ int main()
     }
     for(i=0;i<n;i++)
     {
-        *(a+*(a+i))=*(a+*(a+i)) * -1;
-    }
-    for(i=0;i<n;i++)
-    {
-        printf("%d",*(a+i));
+        m=*(a+i);
+        if(m<0)
+            m*=-1;
+        *(a+m)=*(a+m) * -1;
     }
     for(i=0;i<n;i++)
     {
